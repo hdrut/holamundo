5cb81869bd168caff133dafa660c1034f61e5fcc
Horacio <hdrut@yahoo.com>
2023-07-05 12:24:17 -0300

    Updated path to maven in build.groovy

--- build.groovy
+++ build.groovy
@@ -37,7 +37,7 @@ pipeline{
             steps{
                 sh '''
                     #mvn clean install
-                    /home/jenkins/apache-maven-3.6.3/bin/mvn package
+                    /opt/apache-maven-3.6.3/bin/mvn package
                 '''
             }
         }
